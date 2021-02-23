06877e455dc53dce3c80c0dc1a0e03dc39a91fe7
Maxim <mreksha@gmail.com>
2021-02-23 15:47:56 +0200

    First commit

---
+++ src/hoc/Auxiliary/dist/Auxiliary.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var Auxiliary = function Auxiliary(props) {
+  return props.children;
+};
+
+var _default = Auxiliary;
+exports["default"] = _default;
+//# sourceMappingURL=Auxiliary.dev.js.map
